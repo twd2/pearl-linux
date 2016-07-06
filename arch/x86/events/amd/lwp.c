852c0731840bbf8b35e24b9a55f80891b3b43097
Pip <pipcet@gmail.com>
2016-07-06 22:40:41 +0000

    whitespace

--- arch/x86/events/amd/lwp.c
+++ arch/x86/events/amd/lwp.c
@@ -43,12 +43,12 @@
 static struct pmu pmu;
 
 struct lwp_hw_events {
-	struct perf_event	*event;
+        struct perf_event	*event;
         int                     enabled;
 };
 
 DEFINE_PER_CPU(struct lwp_hw_events, lwp_hw_events) = {
-	.enabled = 1,
+        .enabled = 1,
 };
 
 /*
@@ -86,14 +86,14 @@ static void release_lwp_hardware(void)
 
 static bool check_hw_exists(void)
 {
-	u64 val, val_fail, val_new= ~0;
-	int reg, reg_fail, ret = 0;
-	int bios_fail = 0;
+        u64 val, val_fail, val_new= ~0;
+        int reg, reg_fail, ret = 0;
+        int bios_fail = 0;
 
         /*
-	 * Check to see if the BIOS enabled LWP. If so,
-	 * complain and bail.
-	 */
+         * Check to see if the BIOS enabled LWP. If so,
+         * complain and bail.
+         */
         reg = MSR_AMD64_LWP_CFG;
         ret = rdmsrl_safe(reg, &val);
 
@@ -107,33 +107,33 @@ static bool check_hw_exists(void)
                 goto msr_fail;
         }
 
-	return true;
+        return true;
 
 msr_fail:
-	pr_cont("Broken PMU hardware detected, using software events only.\n");
-	pr_info("%sFailed to access perfctr msr (MSR %x is %Lx)\n",
-		boot_cpu_has(X86_FEATURE_HYPERVISOR) ? KERN_INFO : KERN_ERR,
-		reg, val_new);
+        pr_cont("Broken PMU hardware detected, using software events only.\n");
+        pr_info("%sFailed to access perfctr msr (MSR %x is %Lx)\n",
+                boot_cpu_has(X86_FEATURE_HYPERVISOR) ? KERN_INFO : KERN_ERR,
+                reg, val_new);
 
-	return false;
+        return false;
 }
 
 static void lwp_release_hardware(void);
 
 static void hw_perf_event_destroy(struct perf_event *event)
 {
-	lwp_release_hardware();
-	atomic_dec(&active_events);
+                lwp_release_hardware();
+        atomic_dec(&active_events);
 }
 
 static int lwp_reserve_hardware(void)
 {
-	int err = 0;
+        int err = 0;
 
-        if (!reserve_lwp_hardware())
-                err = -EBUSY;
+			if (!reserve_lwp_hardware())
+				err = -EBUSY;
 
-	return err;
+        return err;
 }
 
 static void lwp_release_hardware(void)
@@ -143,7 +143,7 @@ static void lwp_release_hardware(void)
 
 static int lwp_setup_perfctr(struct perf_event *event)
 {
-	struct perf_event_attr *attr = &event->attr;
+        struct perf_event_attr *attr = &event->attr;
 
         if (attr->type != pmu.type)
                 return -EINVAL;
@@ -151,12 +151,12 @@ static int lwp_setup_perfctr(struct perf_event *event)
         if (attr->exclude_user || !attr->exclude_kernel)
                 return -EOPNOTSUPP;
 
-	return 0;
+        return 0;
 }
 
 static int lwp_hw_config(struct perf_event *event)
 {
-	return lwp_setup_perfctr(event);
+        return lwp_setup_perfctr(event);
 }
 
 /*
@@ -164,24 +164,24 @@ static int lwp_hw_config(struct perf_event *event)
  */
 static int __lwp_event_init(struct perf_event *event)
 {
-	int err;
+        int err;
 
-	err = lwp_reserve_hardware();
-	if (err)
-		return err;
+        err = lwp_reserve_hardware();
+        if (err)
+                return err;
 
-	atomic_inc(&active_events);
-	event->destroy = hw_perf_event_destroy;
+        atomic_inc(&active_events);
+        event->destroy = hw_perf_event_destroy;
 
-	event->hw.idx = -1;
-	event->hw.last_cpu = -1;
-	event->hw.last_tag = ~0ULL;
+        event->hw.idx = -1;
+        event->hw.last_cpu = -1;
+        event->hw.last_tag = ~0ULL;
 
-	/* mark unused */
-	event->hw.extra_reg.idx = EXTRA_REG_NONE;
-	event->hw.branch_reg.idx = EXTRA_REG_NONE;
+        /* mark unused */
+        event->hw.extra_reg.idx = EXTRA_REG_NONE;
+        event->hw.branch_reg.idx = EXTRA_REG_NONE;
 
-	return lwp_hw_config(event);
+        return lwp_hw_config(event);
 }
 
 static void refresh_lwp_cfg(void *ignored);
@@ -190,7 +190,7 @@ static unsigned long lwp_cfg = 0xe000000e;
 
 static void lwp_disable_all(void)
 {
-	struct lwp_hw_events *lwp = this_cpu_ptr(&lwp_hw_events);
+        struct lwp_hw_events *lwp = this_cpu_ptr(&lwp_hw_events);
 
         if (lwp->enabled)
                 lwp->enabled = 0;
@@ -201,7 +201,7 @@ static void lwp_disable_all(void)
 
 static void lwp_disable(struct pmu *pmu)
 {
-	struct lwp_hw_events *lwp = this_cpu_ptr(&lwp_hw_events);
+        struct lwp_hw_events *lwp = this_cpu_ptr(&lwp_hw_events);
 
         if (!lwp->enabled)
                 return;
@@ -219,7 +219,7 @@ static void __lwp_enable_event(struct hw_perf_event *hwc,
 
 static void lwp_enable_all(int added)
 {
-	struct lwp_hw_events *lwp = this_cpu_ptr(&lwp_hw_events);
+        struct lwp_hw_events *lwp = this_cpu_ptr(&lwp_hw_events);
 
         if (lwp->event)
                 __lwp_enable_event(NULL, ARCH_PERFMON_EVENTSEL_ENABLE);
@@ -231,8 +231,8 @@ static void lwp_start(struct perf_event *event, int flags);
 
 static void lwp_enable(struct pmu *pmu)
 {
-	struct lwp_hw_events *lwp = this_cpu_ptr(&lwp_hw_events);
-	struct perf_event *event;
+        struct lwp_hw_events *lwp = this_cpu_ptr(&lwp_hw_events);
+        struct perf_event *event;
 	struct hw_perf_event *hwc;
 
         if (lwp->enabled)
@@ -244,16 +244,16 @@ static void lwp_enable(struct pmu *pmu)
         lwp_start(event, PERF_EF_RELOAD);
 
 	lwp->enabled = 1;
-	barrier();
+        barrier();
 
-	lwp_enable_all(1);
+        lwp_enable_all(1);
 }
 
 void lwp_enable_event(struct perf_event *event)
 {
 	if (__this_cpu_read(lwp_hw_events.enabled))
-		__lwp_enable_event(&event->hw,
-                                   ARCH_PERFMON_EVENTSEL_ENABLE);
+        __lwp_enable_event(&event->hw,
+                           ARCH_PERFMON_EVENTSEL_ENABLE);
 }
 
 /*
@@ -264,20 +264,20 @@ void lwp_enable_event(struct perf_event *event)
  */
 static int lwp_add(struct perf_event *event, int flags)
 {
-	struct lwp_hw_events *lwp = this_cpu_ptr(&lwp_hw_events);
+        struct lwp_hw_events *lwp = this_cpu_ptr(&lwp_hw_events);
 
         lwp->event = event;
 
-	return 0;
+        return 0;
 }
 
 static void lwp_start(struct perf_event *event, int flags)
 {
-	struct lwp_hw_events *lwp = this_cpu_ptr(&lwp_hw_events);
+        struct lwp_hw_events *lwp = this_cpu_ptr(&lwp_hw_events);
 	int idx = 1;
 
         if (!event)
-          return;
+                return;
 
 	//if (WARN_ON_ONCE(!(event->hw.state & PERF_HES_STOPPED)))
 	//	return;
@@ -285,13 +285,13 @@ static void lwp_start(struct perf_event *event, int flags)
 	if (WARN_ON_ONCE(idx == -1))
 		return;
 
-	if (flags & PERF_EF_RELOAD) {
+        if (flags & PERF_EF_RELOAD) {
 		WARN_ON_ONCE(!(event->hw.state & PERF_HES_UPTODATE));
-	}
+        }
 
-	event->hw.state = 0;
+        event->hw.state = 0;
 
-	lwp->event = event;
+        lwp->event = event;
         lwp->enabled = true;
 }
 
@@ -306,19 +306,19 @@ void lwp_stop(struct perf_event *event, int flags)
 
 static void lwp_del(struct perf_event *event, int flags)
 {
-	struct lwp_hw_events *lwp = this_cpu_ptr(&lwp_hw_events);
+        struct lwp_hw_events *lwp = this_cpu_ptr(&lwp_hw_events);
 
         lwp->event = NULL;
 }
 
 int lwp_handle_irq(struct pt_regs *regs)
 {
-	struct perf_sample_data data;
-	struct lwp_hw_events *lwp;
-	struct perf_event *event;
-	int handled = 0;
+        struct perf_sample_data data;
+        struct lwp_hw_events *lwp;
+        struct perf_event *event;
+        int handled = 0;
 
-	lwp = this_cpu_ptr(&lwp_hw_events);
+        lwp = this_cpu_ptr(&lwp_hw_events);
 
         event = lwp->event;
 
@@ -337,35 +337,35 @@ int lwp_handle_irq(struct pt_regs *regs)
                 printk(KERN_WARNING "no event\n");
         }
 
-	if (handled)
-		inc_irq_stat(lwp_irqs);
+        if (handled)
+                inc_irq_stat(lwp_irqs);
 
-	return handled;
+        return handled;
 }
 
 static void
 lwp_interrupt(struct pt_regs *regs)
 {
-	u64 start_clock;
-	u64 finish_clock;
-	int ret;
+        u64 start_clock;
+        u64 finish_clock;
+        int ret;
 
         printk(KERN_WARNING "lwp_interrupt\n");
 
-	/*
-	 * All PMUs/events that share this PMI handler should make sure to
-	 * increment active_events for their events.
-	 */
-	if (!atomic_read(&active_events)) {
+        /*
+         * All PMUs/events that share this PMI handler should make sure to
+         * increment active_events for their events.
+         */
+        if (!atomic_read(&active_events)) {
                 printk(KERN_WARNING "no active events.\n");
-		return;
+                return;
         }
 
-	start_clock = sched_clock();
-	ret = lwp_handle_irq(regs);
-	finish_clock = sched_clock();
+        start_clock = sched_clock();
+        ret = lwp_handle_irq(regs);
+        finish_clock = sched_clock();
 
-	//perf_sample_event_took(finish_clock - start_clock);
+        //perf_sample_event_took(finish_clock - start_clock);
 }
 
 PMU_FORMAT_ATTR(event, "config:0-7");
@@ -376,33 +376,33 @@ static struct attribute *formats_attr[] = {
 };
 
 static struct attribute_group lwp_format_group = {
-	.name = "format",
-	.attrs = formats_attr,
+        .name = "format",
+        .attrs = formats_attr,
 };
 
 EVENT_ATTR(lwp-interrupts,		LWP_INTERRUPTS		);
 
 static struct attribute *events_attr[] = {
         EVENT_PTR(LWP_INTERRUPTS),
-	NULL,
+        NULL,
 };
 
 static struct attribute_group lwp_events_group = {
-	.name = "events",
-	.attrs = events_attr,
+        .name = "events",
+        .attrs = events_attr,
 };
 
 ssize_t lwp_event_sysfs_show(char *page, u64 config, u64 event)
 {
-	ssize_t ret;
+        ssize_t ret;
 
-	/*
-	* We have whole page size to spend and just little data
-	* to write, so we can safely use sprintf.
-	*/
-	ret = sprintf(page, "event=0x00\n");
+        /*
+        * We have whole page size to spend and just little data
+        * to write, so we can safely use sprintf.
+        */
+        ret = sprintf(page, "event=0x00\n");
 
-	return ret;
+        return ret;
 }
 
 static int __init lwp_pmu_init(void)
@@ -427,29 +427,29 @@ static int __init lwp_init(void)
         if (ret)
                 return ret;
 
-	pr_info("Light-Weight Profiling Events: ");
+        pr_info("Light-Weight Profiling Events: ");
 
-	switch (boot_cpu_data.x86_vendor) {
-	case X86_VENDOR_AMD:
-		err = lwp_pmu_init();
-		break;
-	default:
-		err = -ENOTSUPP;
-	}
-	if (err != 0) {
-		pr_cont("no LWP driver.\n");
-		return 0;
-	}
+        switch (boot_cpu_data.x86_vendor) {
+        case X86_VENDOR_AMD:
+                err = lwp_pmu_init();
+                break;
+        default:
+                err = -ENOTSUPP;
+        }
+        if (err != 0) {
+                pr_cont("no LWP driver.\n");
+                return 0;
+        }
 
-	/* sanity check that the hardware exists or is emulated */
-	if (!check_hw_exists())
-		return 0;
+        /* sanity check that the hardware exists or is emulated */
+        if (!check_hw_exists())
+                return 0;
 
-	pr_cont("LWP driver.\n");
+        pr_cont("LWP driver.\n");
 
-	perf_pmu_register(&pmu, "lwp", -1);
+        perf_pmu_register(&pmu, "lwp", -1);
 
-	return 0;
+        return 0;
 }
 
 static void __exit lwp_exit(void)
@@ -459,23 +459,23 @@ static void __exit lwp_exit(void)
 
 static inline void lwp_read(struct perf_event *event)
 {
-	//lwp_event_update(event);
+        //lwp_event_update(event);
 }
 
 static int lwp_event_init(struct perf_event *event)
 {
-	int err;
+        int err;
 
         if (event->attr.type != pmu.type)
                 return -ENOENT;
 
-	err = __lwp_event_init(event);
-	if (err) {
-		if (event->destroy)
-			event->destroy(event);
-	}
+        err = __lwp_event_init(event);
+        if (err) {
+                if (event->destroy)
+                        event->destroy(event);
+        }
 
-	return err;
+        return err;
 }
 
 static void refresh_lwp_cfg(void *cfg_raw)
@@ -499,9 +499,9 @@ static int lwp_event_idx(struct perf_event *event)
 }
 
 static const struct attribute_group *lwp_attr_groups[] = {
-	&lwp_format_group,
-	&lwp_events_group,
-	NULL,
+        &lwp_format_group,
+        &lwp_events_group,
+        NULL,
 };
 
 static void lwp_sched_task(struct perf_event_context *ctx, bool sched_in)
@@ -521,25 +521,25 @@ struct lwp_task_context {
 
 static struct pmu pmu = {
         .task_ctx_nr            = perf_sw_context,
-	.pmu_enable		= lwp_enable,
-	.pmu_disable		= lwp_disable,
+        .pmu_enable		= lwp_enable,
+        .pmu_disable		= lwp_disable,
 
-	.attr_groups		= lwp_attr_groups,
+        .attr_groups		= lwp_attr_groups,
 
-	.event_init		= lwp_event_init,
+        .event_init		= lwp_event_init,
 
-	.event_mapped		= lwp_event_mapped,
-	.event_unmapped		= lwp_event_unmapped,
+        .event_mapped		= lwp_event_mapped,
+        .event_unmapped		= lwp_event_unmapped,
 
-	.add			= lwp_add,
-	.del			= lwp_del,
-	.start			= lwp_start,
-	.stop			= lwp_stop,
-	.read			= lwp_read,
+        .add			= lwp_add,
+        .del			= lwp_del,
+        .start			= lwp_start,
+        .stop			= lwp_stop,
+        .read			= lwp_read,
 
-	.event_idx		= lwp_event_idx,
-	.sched_task		= lwp_sched_task,
-	.task_ctx_size          = sizeof(struct lwp_task_context),
+        .event_idx		= lwp_event_idx,
+        .sched_task		= lwp_sched_task,
+        .task_ctx_size          = sizeof(struct lwp_task_context),
 };
 
 module_init(lwp_init);
