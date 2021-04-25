06850cd9807729df4744ff589c02058e80a8b628
Pip Cet <pipcet@gmail.com>
2021-04-25 22:20:12 +0000

    initialize the CPU in the kernel after secondary entry

--- arch/arm64/kernel/apple_cpustart.c
+++ arch/arm64/kernel/apple_cpustart.c
@@ -34,6 +34,32 @@ static int __init cpu_apple_start_init(unsigned int cpu)
     return 0;
 }
 
+uint32_t real_entry[1024] __attribute__((aligned(65536))) = {
+  0x58000141, /* counter sequence */
+  0x100000a0,
+  0xf9400001,
+  0x91000421,
+  0xf9000001,
+  0x17fffffc,
+  0x00000000,
+  0x00000000,
+  0x58000141,
+  0x52a00200, /* reboot sequence */
+  0xb9001420,
+  0x52800000,
+  0xb9001020,
+  0x52800080,
+  0xb9001c20,
+  0x14000000,
+  0x35200000,
+  0x00000002,
+  0x3d2b0000,
+  0x00000002,
+  0x10000401,
+  0xf9400021,
+  0xd61f0020,
+  0x14000000, /* br . */ };
+
 static int cpu_apple_start_prepare(unsigned int cpu)
 {
     struct device_node *node;
@@ -43,6 +69,24 @@ static int cpu_apple_start_prepare(unsigned int cpu)
 
     if(info->pmgr_start && info->cputrc_rvbar && info->dbg_unlock)
         return 0;
+
+    unsigned long addr = __pa_symbol(secondary_entry);
+    unsigned long addr2 = __pa_symbol(real_entry);
+    volatile uint32_t *p = real_entry;
+    p++;
+    int i = 0;
+    for (i = 0; i < 64; i++) {
+      *p++ = 0xd37ff800; /* x0 <<= 1 */
+      if (((unsigned long) addr) & (1L << (63 - i))) {
+	*p++ = 0xb2400000; /* x0 |= 1 */
+      }
+    }
+    /* *p++ = 0xb2400000; x0 |= 1 */
+    *p++ = 0xd61f0000; /* br x0 */
+    *p++ = addr & 0xffffffff;
+    *p++ = (addr >> 32);
+    p = real_entry;
+    *p++ = 0xd2800000; /* mov x0, #0 */
 
     node = of_find_compatible_node(NULL, NULL, "apple,startcpu");
     if(!node) {
@@ -101,14 +145,22 @@ static int cpu_apple_start_boot(unsigned int cpu)
     if(!info->pmgr_start || !info->cputrc_rvbar || !info->dbg_unlock)
         return -EINVAL;
 
-    writeq(__pa_symbol(secondary_entry) | 1, info->cputrc_rvbar);
+    addr = __pa_symbol(secondary_entry);
+    unsigned long addr2 = __pa_symbol(real_entry);
+    dsb(sy);
+    writeq(addr2, info->cputrc_rvbar);
     readq(info->cputrc_rvbar);
-    writeq(__pa_symbol(secondary_entry) | 1, info->cputrc_rvbar);
+    writeq(addr2, info->cputrc_rvbar);
+    writeq(addr2|1, info->cputrc_rvbar);
+    readq(info->cputrc_rvbar);
+    writeq(addr2|1, info->cputrc_rvbar);
     addr = readq(info->cputrc_rvbar) & 0xFFFFFFFFFul;
     dsb(sy);
 
-    if(addr != (__pa_symbol(secondary_entry) | 1))
-        pr_err("%s: CPU%d reset address: 0x%lx, failed to set to 0x%lx.\n", __func__, cpu, addr, (unsigned long)(__pa_symbol(secondary_entry) | 1));
+    printk("initializing cpustart at %016llx to %016llx (= physaddr(%016llx))\n",
+	   info->cputrc_rvbar, __pa_symbol(secondary_entry), secondary_entry);
+    if(addr != addr2)
+      pr_err("%s: CPU%d reset address: 0x%lx, failed to set to 0x%lx.\n", __func__, cpu, addr, addr2);
 
     writel(MAGIC_UNLOCK, info->dbg_unlock);
 
